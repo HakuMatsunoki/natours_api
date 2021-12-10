640a5cfc5fe0cf532dd98883907fb77450e8d7e9
Haku Matsunoki <ausecours1@gmail.com>
2021-12-10 17:53:04 +0200

    started tours functionality

--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -1,13 +1,32 @@
 import express from "express";
 
 import * as tourController from "../controllers/tourController";
+import * as authMiddleware from "../middlewares/authMiddleware";
+// import * as tourMiddleware from "../middlewares/tourMiddleware";
+import { UserRoles } from "../constants";
 
 const router = express.Router();
 
-router.post("/", tourController.createTour);
 router.get("/", tourController.getAllTours);
-router.get("/:id", tourController.getTour);
-router.patch("/:id", tourController.updateTour);
-router.delete("/:id", tourController.deleteTour);
+router.post(
+  "/",
+  authMiddleware.protectRoute,
+  authMiddleware.restrictTo(UserRoles.ADMIN, UserRoles.LEAD_GUIDE),
+  tourController.createTour
+);
+
+router
+  .route("/:id")
+  .get(tourController.getTour)
+  .patch(
+    authMiddleware.protectRoute,
+    authMiddleware.restrictTo(UserRoles.ADMIN, UserRoles.LEAD_GUIDE),
+    tourController.updateTour
+  )
+  .delete(
+    authMiddleware.protectRoute,
+    authMiddleware.restrictTo(UserRoles.ADMIN, UserRoles.LEAD_GUIDE),
+    tourController.deleteTour
+  );
 
 export { router as tourRouter };
